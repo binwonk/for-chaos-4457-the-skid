5b964c8736a2a7f311de5f0ea0874193a7cde59d
binwonk <80070770+binwonk@users.noreply.github.com>
2023-04-18 23:03:27 +0100

    for the skid ahahahaahah

---
+++ chaosFunctions.lua
@@ -0,0 +1,38 @@
+local chaosFunctions = {}
+
+function chaosFunctions.convertToCFrame(value)
+if typeof(value) == "Vector3" then
+returnValue = CFrame.new(value)
+return returnValue
+end
+end
+
+function chaosFunctions.checkChar()
+if game:GetService("Players").LocalPlayer.Character then
+return true
+else
+return false
+end
+end
+
+function chaosFunctions.getRootPart()
+if not game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Humanoid").RootPart then
+game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Humanoid"):GetPropertyChangedSignal("RootPart"):Wait()
+end
+return game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Humanoid").RootPart
+end
+
+function chaosFunctions.teleport(value)
+local CFramePos = nil;
+if typeof(value) == "Vector3" then
+CFramePos = chaosFunctions.convertToCFrame(value)
+elseif typeof(value) == "CFrame" then
+CFramePos = value
+end
+if chaosFunctions.checkChar() then
+chaosFunctions.getRootPart().CFrame = CFramePos
+return "Success!"
+end
+end
+
+return chaosFunctions
