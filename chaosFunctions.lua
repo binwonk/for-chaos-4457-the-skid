cf685648920205b7a990f9d340e7b64401ef1cc6
binwonk <80070770+binwonk@users.noreply.github.com>
2023-04-19 22:19:07 +0100

    added hook thing

--- chaosFunctions.lua
+++ chaosFunctions.lua
@@ -1,5 +1,11 @@
 local chaosFunctions = {}
 
+local indexHookTable = {}
+local namecallHookTable = {}
+
+chaosFunctions.IndexHook = nil;
+chaosFunctions.NamecallHook = nil;
+
 local LocalPlayer = game:GetService("Players").LocalPlayer
 
 function chaosFunctions.convertToCFrame(value)
@@ -76,6 +82,46 @@ if chaosFunctions.checkChar() then
 chaosFunctions.getRootPart().CFrame = CFramePos
 return "Success!"
 end
+end
+
+chaosFunctions.hookTypes = {
+    Index = "__index",
+    Namecall = "__namecall",
+    NewIndex = "__newindex"
+}
+
+function chaosFunctions.hook(values)
+    values = values or {
+        Metamethod = "look like 4 lines up from here retard",
+        Target = "instance name (or instance path)",
+        Method = "if namecall then put yo thing here (like :Kick for example xdd)",
+        Index = "the index yaya",
+        Function = "function to return instead of the other thing whatever",
+        IgnoreCheckCaller = "true/false" -- NOT AS A STRING
+    }
+    if values.Metamethod == nil then
+        return print("invalid metamethod type... MONKEY!")
+    end
+
+    local oldHook = nil;
+    oldHook = hookmetamethod(game,tostring(values.Metamethod),function(self,...)
+        local args = {...}
+
+        if values.IgnoreCheckCaller and checkcaller() then
+            return oldHook(self,...)
+        end
+        if tostring(self) ~= tostring(values.Target) then
+            return oldHook(self,...)
+        end
+        if values.Metamethod == chaosFunctions.hookTypes.NC and getnamecallmethod() ~= values.Method then
+            return oldHook(self,...)
+        end
+        if values.Metamethod == chaosFunctions.hookTypes.Ind and tostring(args[1]) ~= values.Index then
+            return oldHook(self,...)
+        end
+
+        return values.Function(oldHook, self, args)
+    end)
 end
 
 return chaosFunctions
