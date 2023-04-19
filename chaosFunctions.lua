d59a07e46d5bcaf3eb17a2b90a03f29f40c40364
binwonk <80070770+binwonk@users.noreply.github.com>
2023-04-19 21:46:51 +0100

    added new functoins

--- chaosFunctions.lua
+++ chaosFunctions.lua
@@ -1,5 +1,7 @@
 local chaosFunctions = {}
 
+local LocalPlayer = game:GetService("Players").LocalPlayer
+
 function chaosFunctions.convertToCFrame(value)
 if typeof(value) == "Vector3" then
 returnValue = CFrame.new(value)
@@ -13,6 +15,46 @@ return true
 else
 return false
 end
+end
+
+function chaosFunctions.checkHumanoidRootPart(value)
+    if value:FindFirstChild("HumanoidRootPart") then
+        return true
+    else
+        return false
+    end
+end
+
+function chaosFunctions.checkCharFromPlayer(value)
+    if value.Character then
+        return true
+    end
+end
+
+function chaosFunctions.getClosestPlayers()
+    local Distance = math.huge
+	local ClosestPlayer = nil
+    if chaosFunctions.checkChar() then
+        for i,v in pairs(game:GetService("Players"):GetPlayers()) do
+            if v ~= LocalPlayer and chaosFunctions.checkCharFromPlayer(v) then
+                if chaosFunctions.checkHumanoidRootPart(v.Character) then
+                    local NewDistance = (v.Character.PrimaryPart.Position - LocalPlayer.Character.PrimaryPart.Position).Magnitude
+                    if NewDistance < Distance then
+                        Distance = NewDistance
+                        ClosestPlayer = v
+                    end
+                end
+            end
+        end
+    end
+    return ClosestPlayer
+end
+
+function chaosFunctions.getClosestCharacter()
+    local returnValue = chaosFunctions.getClosestPlayers()
+    if returnValue ~= nil then
+        return returnValue.Character
+    end
 end
 
 function chaosFunctions.getRootPart()
